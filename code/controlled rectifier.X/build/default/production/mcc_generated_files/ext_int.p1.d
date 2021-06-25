316758b3cc49078bb60be669192f6d0fbaeebf79
mhvaleh <64429257+mhvaleh@users.noreply.github.com>
2021-06-25 12:13:02 +0430

    Add files via upload

---
+++ code/controlled rectifier.X/build/default/production/mcc_generated_files/ext_int.p1.d
@@ -0,0 +1,5 @@
+build/default/production/mcc_generated_files/ext_int.p1:  \
+mcc_generated_files/ext_int.c  \
+mcc_generated_files/ext_int.h  \
+mcc_generated_files/../variable.h  \
+mcc_generated_files/tmr2.h 
---
+++ code/controlled rectifier.X/variable.h
@@ -0,0 +1,34 @@
+/* 
+ * File:   variable.h
+ * Author: Valeh
+ *
+ * Created on August 6, 2020, 3:02 PM
+ */
+#define led_red LATC5
+#define led_yellow LATC6
+#define led_green LATC4
+#define thy12 LATA3
+#define thy34 LATD0
+
+
+
+int extintf = 0;
+int cnt1,ctr1 = 0;
+int cnt2,ctr2 = 0;
+
+#ifndef VARIABLE_H
+#define	VARIABLE_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* VARIABLE_H */
+
